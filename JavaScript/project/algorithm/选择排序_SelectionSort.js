169288faff793774504ee626f34ffba47b3ea192
淳员外 <985743464@qq.com>
2023-08-09 22:01:45 +0800

    Add files via upload

---
+++ JavaScript/project/algorithm/选择排序_SelectionSort.js
@@ -0,0 +1,27 @@
+/*
+选择排序（Selection Sort）
+取出一个元素，然后将其他元素和该元素进行比较，如果其他元素比该元素小则交换两个元素的位置
+
+1.算法步骤
+首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
+
+再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+
+重复第二步，直到所有元素均排序完毕。
+*/
+function selectionSort(arr) {
+    var len = arr.length;
+    var minIndex, temp;
+    for (var i = 0; i < len - 1; i++) {
+        minIndex = i;
+        for (var j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {     // 寻找最小的数
+                minIndex = j;                 // 将最小数的索引保存
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    return arr;
+}
